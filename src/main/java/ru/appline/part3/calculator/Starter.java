d5308d7e6ac99f9e96849b6e28ccb4276c9c9b62
Дмитрий <dima-zhumov@yandex.ru>
2020-08-10 00:43:42 +0300

    Exceptions in calculator task7

--- src/main/java/ru/appline/part3/calculator/Starter.java
+++ src/main/java/ru/appline/part3/calculator/Starter.java
@@ -5,16 +5,27 @@ import ru.appline.part3.calculator.operations.Division;
 import ru.appline.part3.calculator.operations.Multiplication;
 import ru.appline.part3.calculator.operations.Subtraction;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Starter {
 
     public static void main(String[] args) {
         Starter starter = new Starter();
-        System.out.println("Hello, my dear friend. What's your name?");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        starter.startCalculator(name);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Hello, my dear friend. What's your name?");
+            String name = reader.readLine();
+            if (name.equals(""))
+                throw new Exception("Name can't be empty");
+            starter.startCalculator(name);
+        } catch (IOException e) {
+            System.out.println("Exception throws: " + e);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -29,7 +40,11 @@ public class Starter {
         System.out.println("Hello, " + name + "! I'm calculator!");
         System.out.println("Input first number?");
         //Инициализируем первое дробное число число
-        calculator.setCurrentStorage(scanner.nextDouble());
+        try {
+            calculator.setCurrentStorage(scanner.nextDouble());
+        } catch (InputMismatchException e) {
+            System.out.println("Exception throws: " + e);
+        }
         scanner.nextLine();
         System.out.println("What's your command? \"+\", \"-\", \"*\", \"/\" or exit");
         //Инициализируем переменную для выбора операции
@@ -53,8 +68,12 @@ public class Starter {
                     System.out.printf("Total: %.4f \n", calculator.getCurrentStorage());
                     break;
                 case "/":
-                    calculator.setCurrentStorage(division.division(calculator.getCurrentStorage(), numberSecond));
-                    System.out.printf("Total: %.4f \n", calculator.getCurrentStorage());
+                    try {
+                        calculator.setCurrentStorage(division.division(calculator.getCurrentStorage(), numberSecond));
+                        System.out.printf("Total: %.4f \n", calculator.getCurrentStorage());
+                    } catch (ArithmeticException e) { //учитывая, что у нас double, исключение выполняться не будет
+                        System.out.println("Can't be divisible by 0");//при делении на 0, будет бесконечность
+                    }
                     break;
                 default:
                     System.out.println("Incorrect command");
